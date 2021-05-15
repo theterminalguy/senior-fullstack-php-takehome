fe74b9a0dc1ba3ebe5262cae807fa3fc5b278d92
theterminalguy <damiansimonpeter@gmail.com>
2021-05-15 15:16:10 -0400

    Add soap server
    - Add logger
    - Implement Service Not Found
    - Implement Service Action Not Found

--- php-soapservice/src/server.php
+++ php-soapservice/src/server.php
@@ -2,8 +2,9 @@
 
 namespace Application;
 
+use SoapServer;
+
 use Application\config\DotEnv;
-use Application\Service\ResponseCode;
 
 (new DotEnv())->load();
 
@@ -15,47 +16,10 @@ final class Logger
     }
 }
 
-class server
-{
-    const NOT_FOUND = 'Service Not Found';
-    const ACTION_NOT_FOUND = 'Action Not Found on Service';
+$options = [
+    'uri' => getenv('SOAP_SERVICE_HOST')
+];
 
-    private $service;
-    private $action;
-    private $args;
-
-    public function handle($request)
-    {
-        if ($_SERVER['HTTP_AUTHORIZATION'] != getenv('SOAP_SERVICE_TOKEN')) {
-            http_response_code(ResponseCode::UN_AUTHORIZED);
-            die(ResponseCode::UN_AUTHORIZED);
-        }
-
-        // Log request to help with debugging
-        Logger::log($request);
-
-        $this->service = $request['service'];
-        $this->action = $request['service'];
-        $this->args = $request['args'];
-
-        if (!class_exists($this->service)) {
-            http_response_code(ResponseCode::NOT_FOUND);
-            die(server::NOT_FOUND);
-        }
-
-        if (!method_exists($this->service, $this->action)) {
-            http_response_code(ResponseCode::NOT_FOUND);
-            die(server::ACTION_NOT_FOUND);
-        }
-
-        $response = call_user_func_array(
-            [
-                new $this->service($this->args),
-                $this->action
-            ],
-            []
-        );
-
-        return json_encode($response);
-    }
-}
+$server = new SoapServer(null, $options);
+$server->setClass('SecureSoapServer');
+$server->handle();
